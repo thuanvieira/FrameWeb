de45a7b3ab926b69d999a971f2764e16fb5b85bf
Thuan Vieira <thuanvieira@gmail.com>
2022-02-04 13:03:34 +0000

    Config basica do backend

---
+++ backend/config/routes.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+module.exports = function(server){
+  //API routes
+  const router = express.Router()
+  server.use('/api', router)
+
+  const billingCycleService = require('../api/billingCycle/billingCycleService')
+  billingCycleService.register(router, '/billingCycles')
+}
